1846998a27e4f22059fecc210cafa20c6782cda1
Mike Wilson <mikewilsonstl@gmail.com>
2023-04-24 23:36:15 -0500

    Succeeded at printing a quote from response body. Entire object was flattened into a string though 🤦‍♂️

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,10 +21,24 @@ public class Main {
 
             HttpResponse<String> response = httpClient.send(request,
                     HttpResponse.BodyHandlers.ofString());
-
+            System.out.println("API Call confirmation:\n");
             System.out.println("Status code: " + response.statusCode());
             System.out.println("Headers: " + response.headers().allValues("content-type"));
-            System.out.println("Body: " + response.body());
+            System.out.println("Body: " + response.body() + "\n");
+
+            System.out.println("Quote of the Day:\n");
+
+            String quote = response.body();
+
+            int contentBeginning = quote.indexOf("content") + 10;
+            int authorBeginning = quote.indexOf("author") + 9;
+            int contentEnd = authorBeginning - 12;
+            int authorEnd = quote.indexOf("tags") - 3;
+
+            String content = quote.substring(contentBeginning, contentEnd);
+            String author = quote.substring(authorBeginning, authorEnd);
+
+            System.out.println(content + " - " + author);
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
